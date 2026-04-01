a80c0f4d704a71f8749e7aaeff09522e0c2c4be2
Pratheeksha27032007 <pratikshand3@gmail.com>
2026-04-01 17:08:19 +0530

    Add render deployment config

--- app.py
+++ app.py
@@ -234,4 +234,5 @@ Keep reply short."""
 # ---------------- RUN ----------------
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
